b9fca7bf54fc17f0123e410bde78a1127ca044fa
Fariz Fakkel (DESKTOP) <fariz_fakkel@hotmail.com>
2018-05-01 06:06:42 +0200

    removed a dot :P

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="gradientoverlay">
       <div id="intro">
         <div class="text">
-          <h1>Simply solid and just perfect for you.</h1>
+          <h1>Simply solid and just perfect for you</h1>
           <p>
             Hi! I'm Fariz, and I'm a <b>software engineering student</b> who is passionate
             about producing clean and concise code to create digital solutions
