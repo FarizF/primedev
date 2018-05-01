e4a3aee63ed5119f0730abc764c149061f03487e
Fariz Fakkel (DESKTOP) <fariz_fakkel@hotmail.com>
2018-05-01 06:02:57 +0200

    Several minor sass edits

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <div class="text">
           <h1>Simply solid and just perfect for you.</h1>
           <p>
-            Hi! I'm Fariz, and I'm a software engineering student who is passionate
+            Hi! I'm Fariz, and I'm a <b>software engineering student</b> who is passionate
             about producing clean and concise code to create digital solutions
             that fulfill any demand. Developing solutions that are ultimately of
             significance is amazingly satisfying.
