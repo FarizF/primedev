57c3459c790819acb3716137f952557f61fcac3a
Fariz Fakkel (DESKTOP) <fariz_fakkel@hotmail.com>
2018-05-01 05:31:25 +0200

    Added font-faces to sass, made layout changes and added content to website.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.master')
 
 @section('content')
-    <div id="photogradient">
+  <div id="homephoto">
+    <div class="gradientoverlay">
+      <div id="intro">
+        <div class="text">
+          <h1>Simply solid and just perfect for you.</h1>
+          <p>
+            Hi! I'm Fariz, and I'm a software engineering student who is passionate
+            about producing clean and concise code to create digital solutions
+            that fulfill any demand. Developing solutions that are ultimately of
+            significance is amazingly satisfying.
+          </p>
+        </div>
+        <div class="verticalline"></div>
+        <div id="mybigface">
 
+        </div>
+      </div>
     </div>
+  </div>
 @endsection
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,3 +1,11 @@
 <nav id="nav">
-    
+  <div class="container">
+    <a id="logolink" href="/"><img src="{{asset('/images/logo_farizfakkel.png')}}"></a>
+    <div id="desktopmenu">
+      <a href="/">About</a>
+      <a href="#">Portfolio</a>
+      <a href="#">Resume</a>
+      <a href="#">Contact</a>
+    </div>
+  </div>
 </nav>
