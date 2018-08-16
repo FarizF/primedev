27525eb53575f7e9f371dccd098347819653eaf8
Fariz Fakkel Desktop <fariz_fakkel@hotmail.com>
2018-08-17 01:26:13 +0200

    Started edits for (responsive) sticky menu

--- resources/assets/js/jcustom.js
+++ resources/assets/js/jcustom.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
   $(".hbbutton img").on("click", function() {
-    if($("#mobilenav").css("display") == "none") {
+    if ($("#mobilenav").css("display") == "none") {
       $("#mobilenav").css("display", "initial");
     } else {
       $("#mobilenav").css("display", "none");
+    }
+  })
+
+  $("body").on("scroll", function() {
+    if (window.innerWidth <= 900) {
+      if (document.scrollTop() > $("#nav").height()) {
+        console.log("stick");
+      }
+    } else {
+      
     }
   })
 });
